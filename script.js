cecc19abdf51c667edb11678d6bd254ca38c8612
Surya <surya.pasumarthi@flynava.ai>
2024-04-26 14:17:58 +0530

    js.txt

--- script.js
+++ script.js
@@ -1,14 +1,85 @@
 
 
 
-// console.log(addExpr(5,4));
+// // // console.log(addExpr(5,4));
 
-var addExpr =function (arg1,arg2){
-    return arg1+arg2
-}
+// // var addExpr =function (arg1,arg2){
+// //     return arg1+arg2
+// // }
 
 
-const x=5;
-var y=6;
+// // const x=5;
+// // var y=6;
 
-console.log(window.y)
+// // console.log(window.y)
+
+
+
+
+
+
+
+// // const calcAge =function (ag1){
+// //     console.log(this);
+// //     console.log(ag1)
+// // }
+// // calcAge(5);
+
+
+
+
+// // const calcAge = (ag1) =>{
+// // console.log(this);
+// // }
+// // calcAge(5);
+
+// // var year =7;
+// function x (){
+//     console.log(this)
+// };
+
+// x();
+
+
+// const function1=function(){
+
+// const jonas ={"name":"jonas",
+// year:1991,
+// calcAge:function(){
+//     console.log(2037-this.year);
+// },
+//     greet:()=>{
+//         var year=5;
+//         console.log(this.year)}
+//     }
+// // func()
+// // console.log(func)
+// jonas.greet();}
+
+
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
+    },
+  };
+
+
+
